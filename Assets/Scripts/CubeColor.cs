e076098c0a153f1c1c6f19d41b7433db1a8da519
jibin John <jibin.johnxr@gmail.com>
2023-10-24 02:36:21 +0530

    Script added to generate and test a conflict

---
+++ Assets/Scripts/CubeColor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("From Branch1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
